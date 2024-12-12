6e95bd2d87bb7cf2178f1a8986c6b57e205d847a
etheoo <etheoo@protonmail.com>
2024-12-12 13:02:16 +0100

    Move persistence logic for deleting user to repository

--- src/Domain/Interfaces/Repositories/IUserRepository.cs
+++ src/Domain/Interfaces/Repositories/IUserRepository.cs
@@ -7,4 +7,5 @@ public interface IUserRepository
     Task<User> AddUser(User user, CancellationToken cancellationToken);
     Task<User> UpdateUserAsync(User user, CancellationToken cancellationToken);
     Task<User?> GetUserByIdAsync(int id, CancellationToken cancellationToken);
+    Task DeleteUser(User user, CancellationToken cancellationToken);
 }
